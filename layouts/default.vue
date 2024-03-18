0c328e691ac14032395872c6b097e56a8753af65
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-03-18 22:15:26 +1100

    Remove unneeded classes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row">
     <div
-      class="flex flex-col items-start justify-start w-full h-screen px-10 py-16"
+      class="w-full h-screen px-10 py-16"
     >
       <slot />
     </div>
