75f298b2e618c58bbe3dc2d64c6068a37d1a5019
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-03-18 22:15:36 +1100

    Refactoring index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,21 @@
 <template>
-  <div>
-    <h1 class="text-6xl font-heading">
-      Hi, I'm <span class="title-gradient">Artie</span>.
-    </h1>
-    <p class="max-w-5xl text-base">
-      I’m from Australia and I have a passion for programming, networking, and
-      anything else relating to computers / technology. I love spending my time
-      with friends, listening to music, reading documentation, and of course
-      programming!
+  <div class="flex flex-col gap-32">
+    <div>
+      <h1 class="text-6xl font-heading">
+        Hi, I'm <span class="title-gradient">Artie</span>.
+      </h1>
+      <p class="text-xl">Software Developer.</p>
+    </div>
 
-      <br />
-      <br />
-
-      I currently work at my friend’s company XtremeFX as the Full-stack
-      Developer / Engineer, there’s been slow progress within the company,
-      though all is well.
-
-      <br />
-      <br />
-
-      If you wish to talk with me, please be nice! I’m shy around new people I’m
-      not familiar with.
-    </p>
+    <div>
+      <h2 class='text-4xl font-heading'>Quick About Me.</h2>
+      <p class="max-w-4xl text-base text-wrap">
+        I’m from Australia and I have a passion for programming, networking, and
+        anything else relating to computers / technology.
+        <br />
+        I love spending my time with friends, listening to music, reading
+        documentation, and of course programming!
+      </p>
+    </div>
   </div>
 </template>
