ae0ae40c98913f438d040561e988b95b24429cfc
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-03-18 22:41:36 +1100

    Add a custom scollbar to main container
    I plan to update this over time

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-row">
+  <div class="flex flex-row scroll-smooth">
     <div
-      class="w-full h-screen px-10 py-16 overflow-y-scroll"
+      class="w-full h-screen px-10 py-16 overflow-y-scroll fancy-scrollbar"
     >
       <slot />
     </div>
