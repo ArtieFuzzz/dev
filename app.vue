63c66064edb6a67da6621b0adb0ad917e1a30f89
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-03-18 16:14:25 +1100

    Replace favicon

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<script lang="ts" setup>
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+})
+</script>
+
 <template>
-    <NuxtLayout>
+  <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
